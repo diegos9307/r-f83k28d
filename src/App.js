ad52f8b1d5a6fb90b71ff53b33875ff5e0aec6c3
Diego Suaza <diegos9307@gmail.com>
2022-03-25 12:15:10 -0500

    Implementación Incrementa el número

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contador: 0,
+    };
+
+    this.sumar = this.sumar.bind(this);
+  }
+
+  sumar(e) {
+    this.setState({
+      contador: this.state.contador + 1,
+    });
+  }
+
   render() {
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{this.state.contador}</span>
+        <button onClick={this.sumar} id="inc">
+          Incrementa
+        </button>
       </div>
     );
   }
